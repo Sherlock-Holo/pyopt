fbac05965247337147237be4ac6290b8d49acbe7
VampireSH <sherlockya@gmail.com>
2017-02-25 01:03:34 +0800

    refine the name

--- src/installed_list.py
+++ src/installed_list.py
@@ -1,7 +1,7 @@
 import subprocess
 
-class list(object):
-    def get_list(self):
+class packages_list(object):
+    def get_packages_list(self):
         bytes = subprocess.check_output(['pacman','-Qq'])
         test = bytes.decode('utf-8')
         self.list = test.split()
