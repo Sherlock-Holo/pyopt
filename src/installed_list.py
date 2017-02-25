2a7377f2a577bd005c3a0fffd8c730299c3a7d08
VampireSH <sherlockya@gmail.com>
2017-02-26 00:08:44 +0800

    rm the old codes

--- src/installed_list.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-class packages_list(object):
-    def get_packages_list(self):
-        bytes = subprocess.check_output(['pacman','-Qq'])
-        test = bytes.decode('utf-8')
-        list = test.split()
-        return list
